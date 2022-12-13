94939cd162b431c26d19526498dff1019ef3c100
Alexis Thierry <alexist103@gmail.com>
2022-12-13 11:51:41 +0100

    fin TP3

--- Math531/TP3/src/Class_PERT.py
+++ Math531/TP3/src/Class_PERT.py
@@ -1,30 +1,53 @@
 class PERT:
-	def __init__(self, root):
-		self.root = root
+    def __init__(self, root):
+        self.root = root
 
-	def critique(self, toStr=False, critique=[]):
-		if not critique:
-			if toStr:
-				critique.append(self.root.get_number())
-			else:
-				critique.append(self.root)
-		if not self.root.get_next_steps():
-			return critique
-		else:
-			for step in self.root.get_next_steps():
-				if step.get_au_plus_tard() == step.get_au_plus_tot():
-					new_step = step
-			if toStr:
-				critique.append(new_step.get_number())
-			else:
-				critique.append(new_step)
-			return PERT(new_step).critique(toStr, critique)
+    def critique(self, toStr=False, critique=[]):
+        """renvoie le chemin critique d'un diagramme PERT
 
-	def compute_au_plus_tot(self):
-		if not self.root.get_previous_steps():
-			self.root.set_au_plus_tot(0)
-		for task in self.root.get_next_tasks():
-			if self.root.get_next_tasks() and (not(task.get_end_step()[0].get_au_plus_tot()) or task.get_end_step()[0].get_au_plus_tot() < task.get_begin_step()[0].get_au_plus_tot() + task.get_duration()):
-				task.get_end_step()[0].set_au_plus_tot(task.get_begin_step()[0].get_au_plus_tot() + task.get_duration())
-			PERT(task.get_end_step()[0]).compute_au_plus_tot()
-			
+        Args:
+                        toStr (bool, optional): permet de renvoyer les noms des objets au lieu des objets eux même. Defaults to False.
+                        critique (list, optional): liste mise a jour. Defaults to [].
+
+        Returns:
+                        list: liste contenant les étapes du chemin critique
+        """
+        if not critique:
+            if toStr:
+                critique.append(self.root.get_number())
+            else:
+                critique.append(self.root)
+        if not self.root.get_next_steps():
+            return critique
+        else:
+            for step in self.root.get_next_steps():
+                if step.get_au_plus_tard() == step.get_au_plus_tot():
+                    new_step = step
+            if toStr:
+                critique.append(new_step.get_number())
+            else:
+                critique.append(new_step)
+            return PERT(new_step).critique(toStr, critique)
+
+    def compute_au_plus_tot(self):
+        """calcule les dates au plus tot de chaque étape pour une diagramme PERT sans dates
+        """
+        if not self.root.get_previous_steps():
+            self.root.set_au_plus_tot(0)
+        for task in self.root.get_next_tasks():
+            if self.root.get_next_tasks() and (not(task.get_end_step()[0].get_au_plus_tot()) or task.get_end_step()[0].get_au_plus_tot() < task.get_begin_step()[0].get_au_plus_tot() + task.get_duration()):
+                task.get_end_step()[0].set_au_plus_tot(task.get_begin_step()[
+                    0].get_au_plus_tot() + task.get_duration())
+            PERT(task.get_end_step()[0]).compute_au_plus_tot()
+
+    def compute_au_plus_tard(self):
+        """calcule les dates au plus tard
+        """
+        self.compute_au_plus_tot()
+        if not self.root.get_next_steps():
+            self.root.set_au_plus_tard(self.root.get_au_plus_tot())
+        for task in self.root.get_next_tasks():
+            duration = task.get_duration()
+            next_step = task.get_end_step()[0]
+            self.root.set_au_plus_tard(next_step.get_au_plus_tot()-duration)
+            PERT(next_step).compute_au_plus_tard()
--- Math531/TP3/src/Class_Task.py
+++ Math531/TP3/src/Class_Task.py
@@ -1,83 +1,88 @@
 from src.Class_Step import *
 
-
 class Task:
-    def __init__(self, name, duration, connection):
+	def __init__(self, name, duration, connection):
+		"""Une tâche avec un nom, une durée et des connections
 
-        self.name = name
-        self.duration = duration
-        # une liste contenant des tuples de la forme (etape_precedante, etape_suivante)
-        self.connection = connection
+		Args:
+			name (str): nom de la tache
+			duration (int): durée de la tache
+			connection (list): liste contenant l'étape de départ et l'étape d'arrivée
+		"""
+		self.name = name
+		self.duration = duration
+		# une liste contenant des tuples de la forme (etape_precedante, etape_suivante)
+		self.connection = connection
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    def get_name(self) -> str:
-        """renvoie le nom de la tâche
+	def get_name(self) -> str:
+		"""renvoie le nom de la tâche
 
-        Returns:
-            toStr: nom de la tâche
-        """
-        return self.name
+		Returns:
+			toStr: nom de la tâche
+		"""
+		return self.name
 
-    def get_duration(self) -> int:
-        """renvoie la durée de la tâche
+	def get_duration(self) -> int:
+		"""renvoie la durée de la tâche
 
-        Returns:
-            int: durée de la tâche
-        """
-        return self.duration
+		Returns:
+			int: durée de la tâche
+		"""
+		return self.duration
 
-    def get_connection(self, toStr=False) -> tuple:
-        """permet de récupérer les étapes à laquelle une tâche est connectée
+	def get_connection(self, toStr=False) -> tuple:
+		"""permet de récupérer les étapes à laquelle une tâche est connectée
 
-        Args:
-            toStr (bool, optional): choisis si l'on renvoit le nom des étapes. Defaults to False.
+		Args:
+			toStr (bool, optional): choisis si l'on renvoit le nom des étapes. Defaults to False.
 
-        Returns:
-            tuple: les étapes connectées par la tâche
-        """
-        if toStr:
-            if self.connection:
-                connection = (self.connection[0].get_number(
-                ), self.connection[1].get_number())
-        else:
-            if self.connection:
-                connection = (self.connection[0], self.connection[1])
-        return connection
+		Returns:
+			tuple: les étapes connectées par la tâche
+		"""
+		if toStr:
+			if self.connection:
+				connection = (self.connection[0].get_number(
+				), self.connection[1].get_number())
+		else:
+			if self.connection:
+				connection = (self.connection[0], self.connection[1])
+		return connection
 
-    def get_begin_step(self, toStr=False):
-        """permet de récupérer l'étape dont la tâche sort
+	def get_begin_step(self, toStr=False):
+		"""permet de récupérer l'étape dont la tâche sort
 
-        Args:
-            toStr (bool, optional): choisis si l'on renvoit le nom des étapes. Defaults to False.
+		Args:
+			toStr (bool, optional): choisis si l'on renvoit le nom des étapes. Defaults to False.
 
-        Returns:
-            l'étape précédente 
-        """
-        previous_steps = []
-        if toStr:
-            if self.connection[0]:
-                next_steps = [self.connection[0].get_number()]
-        else:
-            if self.connection[0]:
-                next_steps = [self.connection[0]]
-        return next_steps
+		Returns:
+			l'étape précédente 
+		"""
+		previous_steps = []
+		if toStr:
+			if self.connection[0]:
+				next_steps = [self.connection[0].get_number()]
+		else:
+			if self.connection[0]:
+				next_steps = [self.connection[0]]
+		return next_steps
 
-    def get_end_step(self, toStr=False) -> list:
-        """permet de récupérer l'étape suivant la tâche
+	def get_end_step(self, toStr=False) -> list:
+		"""permet de récupérer l'étape suivant la tâche
 
-        Args:
-            toStr (bool, optional): choisis si l'on renvoit le nom des étapes. Defaults to False.
+		Args:
+			toStr (bool, optional): choisis si l'on renvoit le nom des étapes. Defaults to False.
 
-        Returns:
-            l'étape suivante
-        """
-        next_steps = []
-        if toStr:
-            if self.connection[1]:
-                next_steps += [self.connection[1].get_number()]
-        else:
-            if self.connection[1]:
-                next_steps += [self.connection[1]]
-        return next_steps
+		Returns:
+			l'étape suivante
+		"""
+		next_steps = []
+		if toStr:
+			if self.connection[1]:
+				next_steps += [self.connection[1].get_number()]
+		else:
+			if self.connection[1]:
+				next_steps += [self.connection[1]]
+		return next_steps
